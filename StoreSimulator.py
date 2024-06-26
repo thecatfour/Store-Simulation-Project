39927a09e3b828b4bf90f0f5f5cf6ba5fd8ef569
thecatfour <atterbur@unlv.nevada.edu>
2024-06-26 16:34:59 -0700

    Started implementation of simulator customer functions.

--- StoreSimulator.py
+++ StoreSimulator.py
@@ -4,8 +4,14 @@ import pandas as pd
 import random as rand
 
 class StoreSimulator:
+
+    # Used when determining what action a customer will take
+    # Sum should be 100 for 100%
+    LOOK_CHANCE = 60
+    BUY_CHANCE = 35
+    LEAVE_CHANCE = 5
     
-    def __init__(self, file_name: str, start_hour: int, end_hour: int, action_interval_minutes: int):
+    def __init__(self, file_name: str, start_hour: float, end_hour: float, action_interval_minutes: int):
         self.item_dataframe = pd.read_csv(file_name)
         
         # Translates the tags into a list
@@ -26,6 +32,14 @@ class StoreSimulator:
         else:
             self.end_hour = end_hour
 
+        # If start hour occurs after end hour, change to back-up values
+        if self.start_hour > self.end_hour:
+            self.start_hour = 9
+            self.end_hour = 17
+
+        self.current_minute = self.start_hour * 60
+        self.day = 1
+
         # Action interval is in minutes
         self.action_interval_minutes = action_interval_minutes
 
@@ -42,25 +56,80 @@ class StoreSimulator:
         # Check if customer with same name is in the store
         # If the customer already exists, create a new object with a "?" added
         if customer.name in self.customers_in_store:
+            print("Warning: customer '" + customer.name + "' is already in the store. Adding '?' to name.")
             customer = cr.Customer(customer.name + " ?", customer.item_tags, customer.money, customer.max_items)
         
+        customer.set_enter(self.current_minute)
         self.customers_in_store[customer.name] = customer
 
-    def customer_leaves(self):
-        pass
+    def customer_leaves(self, customer: cr.Customer):
+        # Create an entry in customer transactions
+        self.customer_transactions.append([str(self.next_transaction_id), customer.name, self.__translate_items_bought(customer.items_bought), "$" + "{:.2f}".format(customer.get_money_difference()), 
+                                           StoreSimulator.minutes_to_time(customer.enter), StoreSimulator.minutes_to_time(self.current_minute)])
+        
+        # Remove that customer from the dict
+        self.customers_in_store.pop(customer.name)
+
+        # Reset their item transactions
+        customer.reset_items()
+
+        # Increment transaction id
+        self.next_transaction_id += 1
+
+    def customer_action(self, customer: cr.Customer):
+        # Check if customer wants to buy something
+        if customer.wants_more_items():
+            # Use rand to determine an action that the customer will take
+            choice = rand.randint(0, StoreSimulator.LOOK_CHANCE + StoreSimulator.BUY_CHANCE + StoreSimulator.LEAVE_CHANCE - 1)
+            
+            if choice < StoreSimulator.LOOK_CHANCE:
+                # Customer does nothing
+                # Buy amounts slightly decreases to keep customers from staying too long
+                customer.decrease_buy_attempts(0.2)
+            elif choice < StoreSimulator.BUY_CHANCE + StoreSimulator.LOOK_CHANCE:
+                # Customer tries to buy something
+                ...
+            else:
+                # Customer decides to leave
+                self.customer_leaves(customer)
+        else:
+            # Customer leaves as they want nothing else
+            self.customer_leaves(customer)
+
+
+    def __translate_items_bought(self, customer_dict: dict) -> list:
+        output = []
+        name_column = self.item_dataframe.Name
+        for key in customer_dict:
+            output.append("Id:" + str(key) + " Name:" + name_column[key] + " Quantity:" + str(customer_dict[key]))
+        return output
+    
+    def check_if_name_in_store(self, name: str) -> bool:
+        return name in self.customers_in_store
+    
+    @staticmethod
+    def minutes_to_time(minutes: int) -> str:
+        return str(int(minutes / 60)) + ":" + str(int(minutes % 60))
 
 
 if __name__ == "__main__":
     print("Simulator Testing\n")
     
-    test_sim = StoreSimulator("ItemList.csv",6,18,5)
+    test_sim = StoreSimulator("ItemList.csv",6.5,18,5)
 
     testCust = cr.Customer()
 
     test_sim.customer_enters(testCust)
     test_sim.customer_enters(testCust)
 
-    print(testCust)
+    testCust.buy_item(1,5)
 
-    for value in test_sim.customers_in_store.values():
-        print(value)
+    test_sim.customer_leaves(testCust)
+    test_sim.customer_enters(testCust)
+    test_sim.customer_leaves(testCust)
+
+    df = pd.DataFrame(test_sim.customer_transactions,columns=["one","two","three","four","five","six"])
+
+    df.to_csv("out.csv",index=False)
+
+    print(df)
