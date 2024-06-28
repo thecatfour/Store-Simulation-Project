f6956b7f5f56fe6e0c23265c127bbd64d6457ac1
thecatfour <atterbur@unlv.nevada.edu>
2024-06-28 00:13:23 -0700

    Added comments and "using credit" to allow negative numbers.

--- Customer.py
+++ Customer.py
@@ -1,20 +1,30 @@
 
 class Customer:
 
-    def __init__(self, name: str = "Default Name", item_tags: list = [], money: float = 10.00, max_buy_attempts: float = 5.0):
+    def __init__(self, name: str = "Default Name", item_tags: list = ["Any"], money: float = 10.00, max_buy_attempts: float = 5.0, using_credit: bool = True):
         self.name = name
+        
+        # Used to determine what customers will buy
         self.item_tags = item_tags
+        
         self.starting_money = money
         self.money = money
         self.items_bought = {}
+
+        # Used to determine the max amount of things the customer can buy
         self.max_buy_attempts = max_buy_attempts
+
+        # Set by a store simulator object of when the customer entered the store (in minutes)
         self.enter = None
+
+        # Used to determine if a customer's money count can go into the negatives
+        self.using_credit = using_credit
 
     def buy_item(self, item_id: int, item_cost: float) -> bool:
         self.max_buy_attempts -= 1
 
         # Check if item can be bought
-        if item_cost <= self.money:
+        if (item_cost <= self.money) or self.using_credit:
             self.money = self.money - item_cost
 
             # Check if customer already bought the item
