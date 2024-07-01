03809b8e5af192b1f3954132e1cb539c3bc73515
thecatfour <atterbur@unlv.nevada.edu>
2024-06-30 20:44:10 -0700

    Added Documentation Added comments to functions.

--- Customer.py
+++ Customer.py
@@ -1,7 +1,23 @@
 
 class Customer:
 
+
+
     def __init__(self, name: str = "Default Name", item_tags: list = ["Any"], money: float = 10.00, max_buy_attempts: float = 5.0, using_credit: bool = True):
+        """
+        Initializes a Customer object.
+
+        name: Name of customer.
+
+        item_tags: Tags that show what items that the customer wants to buy.
+
+        money: Money the customer is willing to spend.
+
+        max_buy_attempts: The max amount of items that a customer is willing to buy.
+
+        using_credit: Allows a customer to spend more money than they brought.
+        """
+
         self.name = name
         
         # Used to determine what customers will buy
@@ -20,7 +36,17 @@ class Customer:
         # Used to determine if a customer's money count can go into the negatives
         self.using_credit = using_credit
 
+
+
     def buy_item(self, item_id: int, item_cost: float) -> bool:
+        """
+        Returns True if the customer bought the inputted item.
+
+        item_id: Id of the item being bought.
+
+        item_cost: Cost of the item being bought.
+        """
+
         self.max_buy_attempts -= 1
 
         # Check if item can be bought
@@ -36,22 +62,96 @@ class Customer:
             return True
         else:
             return False 
+        
+
+
+    def set_max_buy_attempts(self, new_max: float):
+        """
+        Sets the customer's max buy attempts to the input.
+        """
+        
+        self.max_buy_attempts = round(new_max, 1)
+        
+
+
+    def set_money(self, new_money: float):
+        """
+        Sets the customer's money and starting money to the input. Should not be called while the customer is in a store.
+        """
+
+        self.money = round(new_money, 2)
+        self.starting_money = self.money
+
+
+
+    def reset_money_to_starting_money(self):
+        """
+        Sets the customer's money to their starting money.
+        """
+
+        self.money = self.starting_money
+
+
+
+    def set_tags(self, new_tags: list):
+        """
+        Sets the customer's tags to the input.
+        """
+
+        del self.item_tags
+        self.item_tags = new_tags
   
+
+
     def decrease_buy_attempts(self, lower_amount: float):
+        """
+        Decreases customer's max buy attempts by the input.
+        """
+
         self.max_buy_attempts -= lower_amount
 
+
+
     def reset_items(self):
+        """
+        Resets customer's bought items. Should not be called while the customer is in a store.
+        """
+
         del self.items_bought
         self.items_bought = {}
 
+
+
     def get_money_difference(self) -> float:
+        """
+        Returns the difference between the customer's starting money and current money.
+        """
+
         return self.starting_money - self.money
         
+
+
     def wants_more_items(self) -> bool:
+        """
+        Returns True if the customer is still willing to buy more items.
+        """
+        
         return self.max_buy_attempts > 0
     
+
+
     def set_enter(self, enter_time: int):
+        """
+        Sets the minutes of when the customer entered a store. This should only be set by a Store Simulator object.
+        """
+        
         self.enter = enter_time
 
+
+
     def __str__(self) -> str:
-        return "Name:" + self.name + " -- Tags:" + ",".join(self.item_tags) + " -- Money:" + str(self.money) + " -- Bought:" + ",".join(self.items_bought)
+        """
+        toString() for the customer.
+        """
+
+        return "Name: " + self.name + " ; Tags: " + ",".join(self.item_tags) + " ; Money: " + str(self.money) + " ; Bought: " + ",".join(self.items_bought) + " ; Remaining Buys: " + str(self.max_buy_attempts)
